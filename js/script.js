bd28e587acba36e79fb1629ccbec75cd85f90c36
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-05-05 22:46:14 +0000

    done fr now

--- js/script.js
+++ js/script.js
@@ -7,27 +7,36 @@
 "use strict"
 
 /**
- * This function displays all numbers from 0 up to the user number
+ * This function displays all numbers from min up to the max user number
  */
 function displayNumbers() {
-  var min = parseInt(document.getElementById("minInput").value);
-  var max = parseInt(document.getElementById("maxInput").value);
-  var output = document.getElementById("output");
   
-  if (isNaN(min) || isNaN(max)) {
-    output.textContent = "Please enter valid numbers.";
-    return;
+  // Get the minimum and maximum numbers from the user
+  let minNum = parseInt(document.getElementById('minInput').value)
+  let maxNum = parseInt(document.getElementById('maxInput').value)
+
+  // Check if minNum is greater than maxNum
+  if (minNum > maxNum) {
+    document.getElementById('output').innerHTML = "The minimum number has to be less than or equal to the maximum number."
+    return
   }
-  
-  if (min > max) {
-    output.textContent = "Minimum number should be less than or equal to the maximum number.";
-    return;
+
+  // Initialize the counter to the minimum value
+  let counter = minNum
+
+  // Initialize the output string as an empty string
+  let output = ""
+
+  // Use a while loop to generate the numbers between minNum and maxNum
+  while (counter <= maxNum) {
+    
+    //add value of counter to the output
+    output += counter + " "
+    
+    //increment the counter
+    counter = counter + 1
   }
-  
-  var numbers = [];
-  for (var i = min; i <= max; i++) {
-    numbers.push(i);
-  }
-  
-  output.textContent = "Numbers: " + numbers.join(", ");
+
+  // Display the generated numbers in the output 
+  document.getElementById('output').innerHTML = "Numbers: " + output
 }
